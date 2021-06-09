faa12d2257fc755cf0b444bc8823f009dc8fd3bf
ppeums <dlrlqma006@naver.com>
2021-06-09 17:55:28 +0900

    Add member create function

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -21,4 +21,14 @@ public class MemberController {
     public String createForm() {
         return "members/createMemberForm";
     }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 }
